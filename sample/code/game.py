94277f440fe16da4d1f4743ab17ebb34dcc8606e
Vash Ancheta <vashpatrickancheta@gmail.com>
2020-03-21 10:05:14 -0700

    Legal clarification

--- sample/code/game.py
+++ sample/code/game.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-from engine import Controller
-from rmg import RandomMoveGenerator
-from human import HumanPlayer
-
-con = Controller(RandomMoveGenerator(), RandomMoveGenerator())
-conWinner = con.start()
-if conWinner < 3:
-    print(f"Player {conWinner} won!")
-else:
-    print("Draw!")
+print("Lorem ipsum dolor sit amet blindtext blindtext. This is glorious.")
+print("Yes this is blindtext")
+a = 1 + 1
+b = a - 3
+print(b)
